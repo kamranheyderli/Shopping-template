e29ce0268b357e61f30f1e0bb845d933fcfa5720
kamranfrontend <kamranheyderli049@gmail.com>
2023-03-27 00:32:31 +0400

    Remove womens page

--- src/pages/Womens.jsx
+++ src/pages/Womens.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Countdown from '../components/Countdown';
-import Footer from "../components/Footer";
+
+
 
 export const Womens = () => {
   return (
     <div className=' '>
-      {/* <Countdown /> */}
 
-      <Footer />
+
+
     </div>
   )
 };
